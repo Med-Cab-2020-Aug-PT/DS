ec00d25764a46a1ee22b69ccf0102feff84a0162
A.L. Blackwell <ahvblackwelltech@gmail.com>
2020-08-19 23:11:57 -0500

    commit

--- app/app.py
+++ app/app.py
@@ -1,7 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-def read_root():
+def home():
     return {"Hello": "World"}
+
+if __name__ == '__main__':
+    uvicorn.run(app)
