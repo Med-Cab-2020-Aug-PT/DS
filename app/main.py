4ed877d70bd04a53680484c1f746a15e68b0d069
A.L. Blackwell <ahvblackwelltech@gmail.com>
2020-08-22 18:40:36 -0500

    commit

--- app/main.py
+++ app/main.py
@@ -7,5 +7,14 @@ def index():
     return jsonify("API Online!")
 
 
+@API.route("/recommendations", </user_input>)
+def Recommendations():
+    return jsonify(Recommendations)
+
+
+@API.route("/treats")
+def treats():
+    return jsonify("API Online!")
+
 if __name__ == '__main__':
     API.run(debug=True)
