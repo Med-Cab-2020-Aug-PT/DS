eed51c2653fcc630efa21057653878b3edae498b
RAV10K1 <ravindra.tennekone@gmail.com>
2020-08-26 21:12:25 -0700

    redid mongodb

--- app/main.py
+++ app/main.py
@@ -1,7 +1,29 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response
 from app.model import PredictionBot
 
 API = Flask(__name__)
+
+#CORS requirement to access apis - (Courtesy-Jisha)
+@API.route.before_request
+def before_request():
+    """ CORS preflight, required for off-server access """
+
+    def _build_cors_prelight_response():
+        response = make_response()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        response.headers.add("Access-Control-Allow-Headers", "*")
+        response.headers.add("Access-Control-Allow-Methods", "*")
+        return response
+
+    if request.method == "OPTIONS":
+        return _build_cors_prelight_response()
+
+@API.route.after_request
+def after_request(response):
+    """ CORS headers, required for off-server access """
+    header = response.headers
+    header['Access-Control-Allow-Origin'] = '*'
+    return response
 
 @API.route("/")
 def index():
@@ -10,7 +32,6 @@ def index():
 
 @API.route("/recommendations/<web_input>")
 def recommendations(web_input):
-    user_input = request.args[web_input]
     connoissieur = PredictionBot()
     rec_strains = connoissieur.predict(user_input)
     return jsonify(rec_strains)
--- data/mongo.py
+++ data/mongo.py
@@ -5,9 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = getenv("MONGO_USER")
-DB_PW = getenv("MONGO_PW")
-DB_URI = getenv("MONGO_URI")
+# DB_USER = getenv("MONGO_USER")
+# DB_PW = getenv("MONGO_PW")
+# DB_URI = getenv("MONGO_URI")
 
 # client = MongoClient(f"mongodb+srv://{DB_USER}:{DB_PW}@{DB_URI}/strains?retryWrites=true&w=majority")
 # DB = client.strain_table
@@ -23,12 +23,18 @@ def create_db(file_name: str):
     DB_URI = getenv("MONGO_URI")
 
     client = MongoClient(f"mongodb+srv://{DB_USER}:{DB_PW}@{DB_URI}/strains?retryWrites=true&w=majority")
-    DB = client.strain_table
-    table = DB.strain_table
+    DB = client.strains_data
+    table = DB.strains_table
     print(table)
 
     df = pd.read_csv(file_name)
     data = df.to_dict(orient='records')
+    for item in data:
+        item['Effects'] = item['Effects'].split(',')
+        item['Flavors'] = item["Flavors"].split(',')
+        item['Nearest'] = [
+            data[int(idx)]['Name'] for idx in item['Nearest'].split(',')
+        ]
     table.insert_many(data)
 
 if __name__ == "__main__":
