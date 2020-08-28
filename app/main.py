4b7d712b07a48e13e8599e04b5c8ad02ab9d9353
A.L. Blackwell <ahvblackwelltech@gmail.com>
2020-08-27 19:19:02 -0500

    Adding ML2 Model

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
 
+from app.model import PredictionBot
+
 API = Flask(__name__)
+
 
 @API.route("/")
 def index():
     return jsonify("Welcome to Med Cabinet Strain Recommendation API!")
 
 
-@API.route("/recommendations/<user_input>", methods=['GET'] )
+@API.route("/recommendations/<user_input>", methods=['GET'])
 def recommendations(user_input):
     rec_strains = PredictionBot()
     return jsonify(rec_strains.predict(user_input))
--- app/model.py
+++ app/model.py
@@ -1,40 +1,63 @@
 ### Jisha & Robert NLP model for predictions/recommendations
 
-
 """
 NLP Model for DS Build Week
 Input  --> TF-IDF -->  Cosine_Similarity --> Output
-Input  --> TF-IDF -->  Cosine_Similarity --> Output
+Input  --> TF-IDF -->  Nearest Neighbor --> Output
 """
 
-import pandas as pd 
 import os
+from os import getenv
+import pandas as pd
+import pickle
 
 from pymongo import MongoClient
 
+import spacy
+from spacy.tokenizer import Tokenizer
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.neighbors import NearestNeighbors
-from sklearn.metrics.pairwise import cosine_similarity 
+from sklearn.metrics.pairwise import cosine_similarity
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
+FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'csv', 'cannabis.csv')
+DTM_FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'pickled_models', 'dtm.pkl')
+TFIDF_FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'pickled_models', 'tfidf.pkl')
+TOKENIZE_FILEPATH =  os.path.join(os.path.dirname(__file__),'data', 'pickled_models', 'tokenize.pkl')
+
 class PredictionBot:
     """NLP Bot for Cannabis Suggestion App"""
 
-    def predict(self, user_input):
-        return {
-            '_id': 0, 
-            'Name': 'Sour_Diesel', 
-            'Effects': ['High', 'Happy', 'Hungry', 'Sleepy'],
-            'Flavors': ['Sour', 'Fuel', 'Apple'],
-            'Description': 'Sour Diesel is an aphrodisiac that produces a thick cloud', 
-            'Nearest': ['Diesel', 'Sour Apple', 'Cherry Pie', 'Kush', 'Pineapple Express'], 
-            'Rating': 4.7,
-            'Type': 'Hybrid', 
-        }
+    def __init__(self):
+        self.db =  MongoClient(f"{getenv('MONGO_URL')}").medcabinet.strains
+
+        #Pickled models
+        self.tfidf_model = pickle.load(open(TFIDF_FILEPATH, 'rb'))
+        self.dtm_model = pickle.load(open(DTM_FILEPATH, 'rb'))
+        self.tokenize = pickle.load(open(TOKENIZE_FILEPATH, 'rb'))
+
+
+    # def predict(self, user_input):
+    #     return next(self.db.find({'_id': int(self.nearest.kneighbors(
+    #         self.tfidf.transform([user_input]).todense()
+    #         )[1][0][0])}))
+
+    def cosine_recommender(self, user_input):
+        user_dtm = pd.DataFrame(self.tfidf_model.transform([user_input]).todense(), columns=self.tfidf_model.get_feature_names())
+        rec_dtm = self.dtm_model.append(user_dtm).reset_index(drop=True)
+        cosine_df = pd.DataFrame(cosine_similarity(rec_dtm))
+
+        recommendations = cosine_df[cosine_df[0] < 1][0].sort_values(ascending=False)[:1]
+
+        rec_result = recommendations.index.tolist()
+
+        mongo_recs = next(self.db.find({'_id':(rec_result)[0]}))
+        return mongo_recs
 
 if __name__ == "__main__":
     bot = PredictionBot()
-    print(bot.predict("Some text in here.. "))
+    print(bot.cosine_recommender(["Some text in here.. "]))
